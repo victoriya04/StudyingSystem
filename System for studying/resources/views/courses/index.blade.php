bb058235125e2cecf159af3e48bf9ebcb38ef082
Viktoriya Pektova <viktoriya.v.petkova@gmail.com>
2025-01-24 23:25:37 +0200

    Remove create course link from public view

--- System for studying/resources/views/courses/index.blade.php
+++ System for studying/resources/views/courses/index.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
     <h1>Courses List</h1>
-    <a href="{{ route('courses.create') }}">Create New Course</a>
     <ul>
         @foreach ($courses as $course)
             <li>
